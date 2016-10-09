5dd5bc726fddd3006c1cfc4a82252dc17103979e
Roger Bisson <rwbisson@resolvix.com>
2016-10-09 12:01:23 +0100

    Interim commit of work in progress

---
+++ src/main/scala/com/resolvix/ohm/module/jira/JiraModule.scala
@@ -0,0 +1,8 @@
+package com.resolvix.ohm.module.jira
+
+/**
+  * Created by rwbisson on 09/10/16.
+  */
+class JiraModule {
+
+}
---
+++ src/main/scala/com/resolvix/ohm/module/text/TextModule.scala
@@ -0,0 +1,8 @@
+package com.resolvix.ohm.module.text
+
+/**
+  * Created by rwbisson on 08/10/16.
+  */
+class TextModule {
+
+}
