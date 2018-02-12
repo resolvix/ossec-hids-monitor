5aafa40d29d89d86093b1a95a24677c6c9fd23b2
Roger Bisson <rwbisson@resolvix.com>
2018-02-12 01:06:27 +0000

    REFR Minor refactoring

--- src/main/scala/com/resolvix/ccs/api/Transport.scala
+++
@@ -1,8 +0,0 @@
-package com.resolvix.ccs.api
-
-/**
-  * Created by rwbisson on 25/10/16.
-  */
-trait Transport[V] {
-
-}
--- src/main/scala/com/resolvix/sio/api/Actor.scala
+++ src/main/scala/com/resolvix/sio/api/Actor.scala
@@ -3,6 +3,5 @@ package com.resolvix.sio.api
 /**
   * Created by rwbisson on 05/11/2016.
   */
-trait Actor {
+trait Actor
 
-}
